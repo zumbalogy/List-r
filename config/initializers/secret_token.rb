c1c75834fe1d5a9bed719076212c82762aff8067
zumbalogy <intrapostal@gmail.com>
2014-01-11 18:16:28 -0500

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Listr::Application.config.secret_token = 'aea132b1f03e62571b085321e1a3dfac8b3336eaf1afb59821dbe3f56d96a556a3cd62fb74ac384d2a2662cdfa4cdca2143a7ccadec7feaf599f21d3c75c16fd'
